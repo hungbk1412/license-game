f11ae38d8df4e8441f432142dc6d4f01a8c57ba4
Hung NT <hungnt@fabbi.io>
2020-10-14 17:58:28 +0700

    checkCompatibilityComposition and checkCompatibilityCollage

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const constants = require("./utils/constants");
+const utils = require("./utils/utils");
+
+const app = express();
+
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+
+app.post("/", (req, res) => {
+  // console.log('req.body :>> ', req.body);
+  // res.send("Done!");
+  const { type } = req.body;
+  if (type.toLowerCase() === constants.TYPE.collage) {
+    console.log('utils.checkCompatibilityCollage(req.body) :>> ', utils.checkCompatibilityCollage(req.body));
+    res.send(utils.checkCompatibilityCollage(req.body))
+  } else if (type.toLowerCase() === constants.TYPE.composition) {
+    console.log('utils.checkCompatibilityComposition(req.body) :>> ', utils.checkCompatibilityComposition(req.body));
+    res.send(utils.checkCompatibilityComposition(req.body))
+  }
+})
+
+app.listen(6969, () => {
+  console.log("App listening on port 6969");
+})
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,5 @@
+const self = this;
+
 exports.COMPATIBILITY_TABLE = {
   "cc_zero": {
     "cc_zero": "cc_zero",
@@ -63,3 +65,65 @@ exports.COMPATIBILITY_TABLE = {
     "cc_by_nc_nd": false,
   },
 };
+
+exports.LICENSE_POWER_LEVEL = {
+  "cc_zero": 1,
+  "cc_by": 2,
+  "cc_by_sa": 3,
+  "cc_by_nc": 4,
+  "cc_by_nd": 5,
+  "cc_by_nc_sa": 6,
+  "cc_by_nc_nd": 7,
+}
+
+exports.checkCompatibilityComposition = ({licenseArray, clientLicenseAnswer}) => {
+  if (clientLicenseAnswer === false) {
+    return {
+      message: "The answer cannot be false"
+    }
+  }
+  const NC_LICENSES = ["cc_by_nc", "cc_by_nc_nd"];
+  let response = {
+    message: null
+  };
+  for (let i = 0; i < licenseArray.length; i++) {
+    response.licenseFinal = licenseArray[i];
+    if (NC_LICENSES.includes(response.licenseFinal)) {
+      break;
+    }
+  }
+  if (NC_LICENSES.includes(response.licenseFinal) && !NC_LICENSES.includes(clientLicenseAnswer)) {
+    response.message = "The answer must be cc_by_nc or cc_by_nc_nd";    
+  }
+  return response;
+};
+
+exports.checkCompatibilityCollage = ({licenseArray, clientLicenseAnswer}) => {
+  let response = {
+    licenseFinal: licenseArray[0],
+    lastSource: [licenseArray[0]],    
+    message: null
+  };
+  if (licenseArray.length === 1) {
+    if (response.licenseFinal !== clientLicenseAnswer) {
+      response.message = `${response.licenseFinal} !== ${clientLicenseAnswer}`
+    }
+    return response;
+  }
+  for (let i = 1; i < licenseArray.length; i++) {
+    response.lastSource = [response.licenseFinal, licenseArray[i]];
+    response.licenseFinal = self.COMPATIBILITY_TABLE[response.licenseFinal][licenseArray[i]];
+    if (response.licenseFinal === false || self.LICENSE_POWER_LEVEL[response.licenseFinal] > self.LICENSE_POWER_LEVEL[clientLicenseAnswer]) {      
+      break;
+    }
+  }
+  if (response.licenseFinal !== clientLicenseAnswer) {
+    response.message = `${response.lastSource[0]} + ${response.lastSource[1]} = ${response.licenseFinal} instead of ${clientLicenseAnswer}`;
+  }
+  return response;
+};
+
+console.log('object :>> ', self.checkCompatibilityCollage({
+  licenseArray: ["cc_zero", "cc_by_sa"],
+  clientLicenseAnswer: "cc_by"
+}));
