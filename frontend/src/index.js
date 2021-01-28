435d358702d5f0c77f72555127597c775fc0eb3b
vu tuan tran <Vu-Tuan.Tran@aspera.com>
2021-01-28 21:45:15 +0100

    - re-config keycloak at frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,15 +7,13 @@ import {Provider} from 'react-redux';
 import store from "./store";
 import {KEYCLOAK_BASE_URL_API} from "./definitions/config";
 
-let kcConfig = {
-    "realm": "License-game",
-    "auth-server-url": KEYCLOAK_BASE_URL_API + "auth/",
-    "ssl-required": "external",
-    "resource": "react",
-    "public-client": true,
-    "confidential-port": 0
-};
-export const keycloak = Keycloak('/keycloak.json');
+
+export const keycloak = Keycloak({
+    url: KEYCLOAK_BASE_URL_API + "auth/",
+    realm: 'License-game',
+    clientId: 'react'
+});
+
 keycloak
     .init({onLoad: 'login-required'})
     .then((authenticated) => {
