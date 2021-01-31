dfe1c1cd8227400005cca1b6ac65671aaaebfcc4
vu tuan tran <Vu-Tuan.Tran@aspera.com>
2021-01-31 02:09:35 +0100

    - test cause enough from the clown

--- frontend/src/definitions/config/index.js
+++ frontend/src/definitions/config/index.js
@@ -1,5 +1,5 @@
-let api_version = '/api/v1';
-let BACKEND_BASE_URL_API = 'http://localhost/backend' + api_version;
+let api_version = 'api/v1';
+let BACKEND_BASE_URL_API = 'http://localhost/backend/' + api_version;
 let KEYCLOAK_BASE_URL_API = 'http://localhost/keycloak/';
 if (process.env.REACT_APP_KEYCLOAK_BASE_URL) {
     KEYCLOAK_BASE_URL_API = process.env.REACT_APP_KEYCLOAK_BASE_URL;
